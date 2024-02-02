5c3d060a51d95bab46fd48a89bff86b24698385c
Charlotte Smith <csmith@totallymoney.com>
2024-02-02 10:32:30 +0000

    Add ppx deriving

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,17 @@
-let () = print_endline "Hello, World!"
+open Format
+
+module Config = struct
+  type t =
+    { a : int
+    ; b : string
+    ; c : float
+    }
+  [@@deriving show]
+
+  let make ?(a = 1) ?(b = "two") ?(c = 3.0) () = { a; b; c }
+end
+
+let () =
+  let config = Config.make ~c:2.5 () in
+  printf "%a" Config.pp config
+;;
--- pulumi/Program.fs
+++ pulumi/Program.fs
@@ -2,15 +2,24 @@
 
 open Pulumi.FSharp
 open Pulumi.Aws.S3
+open Pulumi.Aws
 
 let infra () =
+    let endpoint = ApiGateway.RestApi("endpoint")
+    let resource = ApiGateway.Resource("resource", ApiGateway.ResourceArgs(
+        ParentId = endpoint.RootResourceId,
+        PathPart = "example",
+        RestApi = endpoint.Id
+    ))
 
-  // Create an AWS resource (S3 Bucket)
-  let bucket = Bucket "my-bucket"
+    let fn =
+        Lambda.Function("main", Lambda.FunctionArgs(), Pulumi.CustomResourceOptions())
 
-  // Export the name of the bucket
-  dict [("bucketName", bucket.Id :> obj)]
+    // Create an AWS resource (S3 Bucket)
+    let bucket = Bucket "my-bucket"
+
+    // Export the name of the bucket
+    dict [ ("bucketName", bucket.Id :> obj) ]
 
 [<EntryPoint>]
-let main _ =
-  Deployment.run infra
+let main _ = Deployment.run infra
